8e6a12d2e37702f0d6349b29504cb8c2025d0c23
Evgeni Pandurski <epandurski@gmail.com>
2019-04-18 14:06:39 +0300

    Add tasks container

--- {{cookiecutter.app_slug}}/docker/entrypoint.sh
+++ {{cookiecutter.app_slug}}/docker/entrypoint.sh
@@ -4,6 +4,16 @@ set -e
 logging_conf="$APP_ROOT_DIR/logging.conf"
 gunicorn_conf="$APP_ROOT_DIR/gunicorn.conf"
 
+# We should allow the container to work with RabbitMQ server installed
+# on the host. To do this, we find the IP address of the host, and
+# then in "$DRAMATIQ_BROKER_URL" we substitute each occurrence of the
+# special hostname "host.docker.internal" with that IP address.
+if [[ -n "$DRAMATIQ_BROKER_URL" ]]; then
+    host_ip=$(ip route show | awk '/default/ {print $3}')
+    export DRAMATIQ_BROKER_URL=${DRAMATIQ_BROKER_URL//host.docker.internal/$host_ip};
+fi
+
+
 case $1 in
     develop)
         # autoreload on
@@ -30,6 +40,10 @@ case $1 in
         ;;
     supervisord)
         exec supervisord -c "$APP_ROOT_DIR/supervisord.conf"
+        ;;
+    tasks)
+        shift;
+        exec dramatiq --processes ${DRAMATIQ_PROCESSES-4} --threads ${DRAMATIQ_THREADS-8} "$@"
         ;;
     *)
         exec "$@"
--- {{cookiecutter.app_slug}}/tasks.py
+++ {{cookiecutter.app_slug}}/tasks.py
@@ -5,3 +5,13 @@ from {{cookiecutter.app_slug}}.extensions import broker
 
 app = create_app()
 broker.set_default()
+
+
+if __name__ == '__main__':
+    import sys
+    print(
+        "This script is intended to be imported by Dramatiq's CLI tools.\n"
+        "\n"
+        "Usage: dramatiq tasks:BROKER_NAME\n"
+    )
+    sys.exit(1)
