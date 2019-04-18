8a294c27f5b48ffac8b23230d801aa4d7d43aa9b
Evgeni Pandurski <epandurski@gmail.com>
2019-04-18 16:24:28 +0300

    Import all tasks in tasks.py

--- {{cookiecutter.app_slug}}/tasks.py
+++ {{cookiecutter.app_slug}}/tasks.py
@@ -2,6 +2,7 @@
 
 from {{cookiecutter.app_slug}} import create_app
 from {{cookiecutter.app_slug}}.extensions import broker
+import {{cookiecutter.app_slug}}.tasks  # noqa
 
 app = create_app()
 broker.set_default()
