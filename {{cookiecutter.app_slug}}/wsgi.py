0defa25a71f94b68bb6d7b28b712e8245298aca8
Evgeni Pandurski <epandurski@gmail.com>
2019-04-22 20:20:02 +0300

    Change default port when running wsgi.py

--- {{cookiecutter.app_slug}}/wsgi.py
+++ {{cookiecutter.app_slug}}/wsgi.py
@@ -5,4 +5,4 @@ from {{cookiecutter.app_slug}} import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=app.config.get('PORT', 8000), debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=app.config.get('PORT', 5000), debug=True, use_reloader=False)
