64705eaac0de64fb1b746d5aa5a261eeed46501e
Evgeni Pandurski <epandurski@gmail.com>
2019-04-22 20:33:01 +0300

    Fix bug in wsgi.py

--- {{cookiecutter.app_slug}}/wsgi.py
+++ {{cookiecutter.app_slug}}/wsgi.py
@@ -5,4 +5,6 @@ from {{cookiecutter.app_slug}} import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=app.config.get('PORT', 5000), debug=True, use_reloader=False)
+    import os
+
+    app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000), debug=True, use_reloader=False)
