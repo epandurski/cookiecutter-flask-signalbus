eb3568c4508d042c16af87c1b6a9fed1857ca085
Evgeni Pandurski <epandurski@gmail.com>
2019-04-29 20:31:34 +0300

    Add "add_emacs_files" option

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,19 +8,7 @@ def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
-if __name__ == 'XXXXXXX__main__':
-{% raw %}
-    if '{{ cookiecutter.create_author_file }}' != 'y':
-        remove_file('AUTHORS.rst')
-        remove_file('docs/authors.rst')
-
-    if '{{ cookiecutter.use_pytest }}' == 'y':
-        remove_file('tests/__init__.py')
-
-    if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
-        cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
-        remove_file(cli_file)
-
-    if 'Not open source' == '{{ cookiecutter.open_source_license }}':
-        remove_file('LICENSE')
-{% endraw %}
+if __name__ == '__main__':
+    if '{{ cookiecutter.add_emacs_files }}' == 'n':
+        remove_file('.projectile')
+        remove_file('.dir-locals.el')
