c44a4d740bf4722c70327abf0ffa29a1ba9134aa
Evgeni Pandurski <epandurski@gmail.com>
2019-06-21 16:46:10 +0300

    Add cli.py

---
+++ {{cookiecutter.app_slug}}/{{cookiecutter.app_slug}}/cli.py
@@ -0,0 +1,16 @@
+import click
+from flask.cli import with_appcontext
+
+
+@click.group()
+def {{cookiecutter.app_slug}}():
+    """Perform {{cookiecutter.app_name}} specific operations."""
+
+
+# Define your CLI commands here. For example:
+#
+#     @{{cookiecutter.app_slug}}.command()
+#     @with_appcontext
+#     @click.option('-o', '--option', type=str, help='An option description')
+#     def do_something(processes):
+#         """Does something."""
